d59569eabf57953ff58a3bcbd89f9dc9c285362e
FeiShepherd <thomasshepherd2002@gmail.com>
2014-11-19 21:56:47 -0500

    api update

--- routes/api.js
+++ routes/api.js
@@ -1,22 +1,39 @@
 var express = require('express');
+var url = require('url');
 var router = express.Router();
 
 
-var temp_db = [
-	{name:"john"},
-	{name:"kyle"},
-	{name:"max"}
+var t_db = [
+	{name:"john",age:"5"},
+	{name:"kyle",age:"10"},
+	{name:"max",age:"15"},
+	{name:"mark",age:"10"},
+	{name:"alex",age:"1"},
+	{name:"thomas",age:"100"}
 ];
-/* GET home page. */
+
 router.get('/', function(req, res) {
-  res.end("welcome to the api \n api/users/2 \n"+temp_db.toString());
+  res.end("welcome to the api \n /api/users/name/2 \n"+t_db.toString());
 });
-router.get('/users/:id',function(req,res){
-  if(req.params.id<temp_db.length){
-    res.json(temp_db[req.params.id]);
-  }else{
-    res.end("error user does not exist");
-  }
-});
+
+var msg_exist = "Does not exist";
+
+
+var type_db = ["age","name"];
+for(i=0;i<type_db.length;i++){
+  var urlget = "/users/"+type_db[i]+"/:id";
+  router.get(urlget,function(req,res){
+    var id = req.params.id;
+    var url_parts = url.parse(req.url,true);
+    var type_array = url_parts.path.split("/");
+    var type = type_array[2];
+    if(id<t_db.length&&id>=0){
+      res.json(t_db[id][type]);
+    }else{
+      res.end(msg_exist);
+    }
+  });
+}
+
 
 module.exports = router;
